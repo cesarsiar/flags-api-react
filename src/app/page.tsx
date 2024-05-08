b978384a0fb27681f790be74dc59dc7a3900edca
saac08 <cesar.silva@coppel.com>
2024-05-08 17:32:51 -0600

    Create Files Interceptor, apiFetch, errorValidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 	return (
 		<Container size={'4'} align={'center'}>
 			<Flex justify={'center'} direction={'column'}>
-				<Text>Hola Mundo</Text>
+				<Text size={'6'}>Hola Mundo</Text>
 			</Flex>
 		</Container>
 	);
