dd4b8e6ec728303f4c588d086b1ce7b8411ea298
Cesar Silva <silvaarvizucesaralejandro@gmail.com>
2024-05-07 17:59:35 -0700

    Added Axios, RadixUI and RadixIcons (#1)
    * Add Axios, RadixUI, RadixUI-Icons
    
    * Setting Up RadixUI
    
    ---------
    
    Co-authored-by: saac08 <cesar.silva@coppel.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<Theme accentColor='cyan'>
+					<ThemePanel />
+					{children}
+				</Theme>
+			</body>
 		</html>
 	);
 }
